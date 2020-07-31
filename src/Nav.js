8c620896273ac3cd460dee9ec23d589868aab910
tushant1037 <42100482+tushant1037@users.noreply.github.com>
2020-07-31 19:46:07 +0530

    Done

---
+++ src/Nav.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://ih1.redbubble.net/image.908933355.7118/pp,840x830-pad,1000x1000,f8f8f8.jpg"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
